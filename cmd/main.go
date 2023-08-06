ae82005dc8492907ad8e7d06019a74cbdff09577
Stone-afk <1711865140@qq.com>
2023-08-06 16:18:30 +0800

    feature: add framework

---
+++ cmd/main.go
@@ -0,0 +1,94 @@
+package cmd
+
+import (
+	"ebook/cmd/internal/handler"
+	"ebook/cmd/internal/repository"
+	"ebook/cmd/internal/repository/cache"
+	"ebook/cmd/internal/repository/dao"
+	"ebook/cmd/internal/service"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"strings"
+	"time"
+)
+
+func main() {
+	db := initDB()
+	rc := initRedis()
+	u := initUser(db, rc)
+
+	server := initServer()
+	u.RegisterRoutes(server)
+
+	err := server.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initServer() *gin.Engine {
+	server := gin.Default()
+
+	// 跨域拦截器
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins: []string{"*"},
+		//AllowMethods: []string{"POST", "GET"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		//ExposeHeaders: []string{"x-jwt-token"},
+		// 是否允许你带 cookie 之类的东西
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// 你的开发环境
+				return true
+			}
+			return strings.Contains(origin, "company.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
+	// 步骤1
+	store := cookie.NewStore([]byte("secret"))
+	server.Use(sessions.Sessions("mysession", store))
+
+	return server
+
+}
+
+func initUser(db *gorm.DB, rcCmd redis.Cmdable) *handler.UserHandler {
+	rc := cache.NewRedisUserCache(rcCmd)
+	ud := dao.NewGORMUserDAO(db)
+	repo := repository.NewUserRepository(ud, rc)
+	svc := service.NewUserService(repo)
+	u := handler.NewUserHandler(svc)
+	return u
+}
+
+func initDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(localhost:13316)/ebook"))
+	if err != nil {
+		// 我只会在初始化过程中 panic
+		// panic 相当于整个 goroutine 结束
+		// 一旦初始化过程出错，应用就不要启动了
+		panic(err)
+	}
+
+	err = dao.InitTables(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initRedis() redis.Cmdable {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:16379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module ebook
 go 1.20
 
 require (
+	github.com/dlclark/regexp2 v1.10.0
+	github.com/ecodeclub/ekit v0.0.7
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-contrib/sessions v0.0.5
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-sql-driver/mysql v1.7.1
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/redis/go-redis/v9 v9.0.5
 	golang.org/x/crypto v0.9.0
+	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.2
 )
 
