88c863357f73e4f67dc3d164761399ce3d13a85e
Stone-afk <1711865140@qq.com>
2023-08-14 21:47:11 +0800

    feature: add limit

--- cmd/pkg/ginx/middleware/ratelimit/builder.go
+++ cmd/pkg/ginx/middleware/ratelimit/builder.go
@@ -1,1 +1,34 @@
 package ratelimit
+
+import (
+	_ "embed"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+type Builder struct {
+	prefix string
+	// 阈值
+	rate     int
+	cmd      redis.Cmdable
+	interval time.Duration
+}
+
+//go:embed slide_window.lua
+var luaScript string
+
+func NewBuilder(cmd redis.Cmdable, interval time.Duration, rate int) *Builder {
+	return &Builder{
+		prefix:   "ip-limiter",
+		rate:     rate,
+		cmd:      cmd,
+		interval: interval,
+	}
+}
+
+func (b *Builder) Build() gin.HandlerFunc {
+	return func(context *gin.Context) {
+
+	}
+}
