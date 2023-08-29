7e69ff21c6dc6ff22688cf89235e9f0c8e1d27fb
Stone-afk <1711865140@qq.com>
2023-08-29 23:35:48 +0800

    add sms service tencent

--- cmd/internal/service/sms/cloopen/service.go
+++ cmd/internal/service/sms/cloopen/service.go
@@ -5,7 +5,9 @@ package cloopen
 
 import (
 	"context"
+	"fmt"
 	"github.com/cloopen/go-sms-sdk/cloopen"
+	"log"
 )
 
 type Service struct {
@@ -21,5 +23,27 @@ func NewService(c *cloopen.SMS, addId string) *Service {
 }
 
 func (s *Service) Send(ctx context.Context, tplId string, data []string, numbers ...string) error {
-	panic("")
+	input := &cloopen.SendRequest{
+		// 应用的APPID
+		AppId: s.appId,
+		// 模版ID
+		TemplateId: tplId,
+		// 模版变量内容 非必填
+		Datas: data,
+	}
+	for _, number := range numbers {
+		input.To = number
+
+		resp, err := s.client.Send(input)
+		if err != nil {
+			return err
+		}
+
+		if resp.StatusCode != "000000" {
+			log.Printf("response code: %s, msg: %s \n", resp.StatusCode, resp.StatusMsg)
+			return fmt.Errorf("发送失败，code: %s, 原因：%s",
+				resp.StatusCode, resp.StatusMsg)
+		}
+	}
+	return nil
 }
