90aaa3da211b39893383a0cc2738e6205bbb90af
Stone-afk <1711865140@qq.com>
2023-09-02 00:16:34 +0800

    add sms code lua

--- cmd/internal/service/code.go
+++ cmd/internal/service/code.go
@@ -1,17 +1,50 @@
 package service
 
 import (
+	"context"
 	"ebook/cmd/internal/repository"
 	"ebook/cmd/internal/service/sms"
+	"fmt"
+	"math/rand"
 )
 
-type CodeService struct {
-	repo   *repository.CodeRepository
-	smsSvc sms.Service
-	//tplId string
+var (
+	ErrCodeVerifyTooManyTimes = repository.ErrCodeVerifyTooManyTimes
+	ErrCodeSendTooMany        = repository.ErrCodeSendTooMany
+)
+
+// CodeService biz 区别业务场景
+type CodeService interface {
+	Send(ctx context.Context, biz string, phone string) error
+	Verify(ctx context.Context, biz string, phone string, inputCode string) (bool, error)
 }
 
-type NamedArg struct {
-	Val  string
-	Name string
+type codeService struct {
+	repo   repository.CodeRepository
+	smsSvc sms.Service
+}
+
+func NewCodeService(repo repository.CodeRepository, smsSvc sms.Service) CodeService {
+	return &codeService{
+		repo:   repo,
+		smsSvc: smsSvc,
+	}
+}
+
+// Send 发验证码，我需要什么参数？
+func (svc *codeService) Send(ctx context.Context, biz string, phone string) error {
+	panic("")
+}
+
+func (svc *codeService) Verify(ctx context.Context, biz string, phone string, inputCode string) (bool, error) {
+	panic("")
+}
+
+func (svc *codeService) generateCode() string {
+	// 六位数，num 在 0, 999999 之间，包含 0 和 999999
+	num := rand.Intn(1000000)
+	// 不够六位的，加上前导 0
+	// 000001
+	// 000001
+	return fmt.Sprintf("%06d", num)
 }
