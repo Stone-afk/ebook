7e8dc4c5c0fe5e94f9a53dc0c70c372f65d3b691
Stone-afk <1711865140@qq.com>
2023-09-02 00:02:16 +0800

    add sms code lua

--- cmd/internal/repository/code.go
+++ cmd/internal/repository/code.go
@@ -1,4 +1,34 @@
 package repository
 
-type CodeRepository struct {
+import (
+	"context"
+	"ebook/cmd/internal/repository/cache"
+)
+
+var (
+	ErrCodeSendTooMany        = cache.ErrCodeSendTooMany
+	ErrCodeVerifyTooManyTimes = cache.ErrCodeVerifyTooManyTimes
+)
+
+type CodeRepository interface {
+	Store(ctx context.Context, biz string, phone string, code string) error
+	Verify(ctx context.Context, biz, phone, inputCode string) (bool, error)
+}
+
+type CachedCodeRepository struct {
+	cache cache.CodeCache
+}
+
+func NewCodeRepository(c cache.CodeCache) CodeRepository {
+	return &CachedCodeRepository{
+		cache: c,
+	}
+}
+
+func (repo *CachedCodeRepository) Store(ctx context.Context, biz string, phone string, code string) error {
+	panic("")
+}
+
+func (repo *CachedCodeRepository) Verify(ctx context.Context, biz, phone, inputCode string) (bool, error) {
+	panic("")
 }
