548061388a087a6529d6bbf0a2c72da2939a7f61
Stone-afk <1711865140@qq.com>
2023-09-08 23:49:04 +0800

    add sms code

--- cmd/ioc/handler.go
+++ cmd/ioc/handler.go
@@ -1,16 +1,44 @@
 package ioc
 
 import (
+	"ebook/cmd/internal/handler"
+	"ebook/cmd/internal/handler/middleware"
+	"ebook/cmd/pkg/ginx/middleware/ratelimit"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/memstore"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
+	"strings"
+	"time"
 )
 
-func InitMiddlewares(redisClient redis.Cmdable) []gin.HandlerFunc {
-	return []gin.HandlerFunc{}
+func InitMiddlewares(redisCmd redis.Cmdable) []gin.HandlerFunc {
+	return []gin.HandlerFunc{
+		corsHdl(),
+		sessions.Sessions("SESS", memstore.NewStore(handler.SessAuthKey, handler.SessEncryptionKey)),
+		middleware.NewJWTLoginMiddlewareBuilder().Build(),
+		ratelimit.NewBuilder(redisCmd, time.Second, 100).Build(),
+	}
 }
 
 func corsHdl() gin.HandlerFunc {
-	return func(context *gin.Context) {
-
-	}
+	// 跨域拦截器
+	return cors.New(cors.Config{
+		//AllowOrigins: []string{"*"},
+		//AllowMethods: []string{"POST", "GET"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		// 不加这个，前端是拿不到 jwt
+		ExposeHeaders: []string{"x-jwt-token"},
+		// 是否允许你带 cookie 之类的东西
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// 你的开发环境
+				return true
+			}
+			return strings.Contains(origin, "company.com")
+		},
+		MaxAge: 12 * time.Hour,
+	})
 }
