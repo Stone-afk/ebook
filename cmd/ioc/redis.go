b63d31ff74c30461283254f0da818b940fa384c9
Stone-afk <1711865140@qq.com>
2023-09-08 21:39:58 +0800

    add sms code

--- cmd/ioc/redis.go
+++ cmd/ioc/redis.go
@@ -1,1 +1,13 @@
 package ioc
+
+import (
+	"ebook/cmd/config"
+	"github.com/redis/go-redis/v9"
+)
+
+func InitRedis() redis.Cmdable {
+	redisCmd := redis.NewClient(&redis.Options{
+		Addr: config.Config.Redis.Addr,
+	})
+	return redisCmd
+}
--- cmd/ioc/sms.go
+++ cmd/ioc/sms.go
@@ -1,1 +1,12 @@
 package ioc
+
+import (
+	"ebook/cmd/internal/service/sms"
+	"ebook/cmd/internal/service/sms/memory"
+	"github.com/redis/go-redis/v9"
+)
+
+func InitSMSService(cmd redis.Cmdable) sms.Service {
+	// 换内存，还是换别的
+	return memory.NewService()
+}
