37600a4bbbe840ea19bca944304423684f33e156
Stone-afk <1711865140@qq.com>
2023-09-08 21:54:27 +0800

    add sms code

--- cmd/ioc/handler.go
+++ cmd/ioc/handler.go
@@ -1,1 +1,16 @@
 package ioc
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+
+func InitMiddlewares(redisClient redis.Cmdable) []gin.HandlerFunc {
+	return []gin.HandlerFunc{}
+}
+
+func corsHdl() gin.HandlerFunc {
+	return func(context *gin.Context) {
+
+	}
+}
