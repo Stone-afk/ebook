27a30dc3d5ca06da07ff7d6af141e5c20b946e60
Stone-afk <1711865140@qq.com>
2023-09-17 18:26:41 +0800

    feat: add user test

---
+++ cmd/internal/service/sms/failover/timeout_service.go
@@ -0,0 +1,61 @@
+package failover
+
+import (
+	"context"
+	"ebook/cmd/internal/service/sms"
+	"sync/atomic"
+)
+
+type TimeoutService struct {
+	// 你的服务商
+	services []sms.Service
+	idx      int32
+	// 连续超时的个数
+	cnt int32
+
+	// 阈值
+	// 连续超时超过这个数字，就要切换
+	threshold int32
+}
+
+func NewTimeoutService(services []sms.Service, threshold int32) sms.Service {
+	return &TimeoutService{
+		services:  services,
+		threshold: threshold,
+	}
+}
+
+func (s *TimeoutService) Send(ctx context.Context,
+	tpl string, args []string, numbers ...string) error {
+	idx := atomic.LoadInt32(&s.idx)
+	cnt := atomic.LoadInt32(&s.cnt)
+	if cnt > s.threshold {
+		// 这里要切换，新的下标，往后挪了一个
+		newIdx := (idx + 1) % int32(len(s.services))
+		if atomic.CompareAndSwapInt32(&s.idx, idx, newIdx) {
+			// 成功往后挪了一位
+			atomic.StoreInt32(&s.cnt, 0)
+		}
+		// else 就是出现并发，别人换成功了
+		//idx = newIdx
+		idx = atomic.LoadInt32(&s.idx)
+	}
+	svc := s.services[idx]
+	err := svc.Send(ctx, tpl, args, numbers...)
+	switch err {
+	case nil:
+		// 发送成功，连续失败的状态被打断了
+		atomic.StoreInt32(&s.cnt, 0)
+		return nil
+	case context.DeadlineExceeded, context.Canceled:
+		// 超时/被上游Cancel
+		atomic.AddInt32(&s.cnt, 1)
+		return err
+	default:
+		// 不知道什么错误
+		// 你可以考虑，换下一个，语义则是：
+		// - 超时错误，可能是偶发的，我尽量再试试
+		// - 非超时，我直接下一个
+		return err
+	}
+}
---
+++ cmd/internal/service/sms/ratelimit/service.go
@@ -0,0 +1,45 @@
+package ratelimit
+
+import (
+	"context"
+	"ebook/cmd/internal/service/sms"
+	"ebook/cmd/pkg/ratelimit"
+	"fmt"
+)
+
+var errLimited = fmt.Errorf("触发了限流")
+
+type Service struct {
+	key     string // 服务商地址
+	svc     sms.Service
+	limiter ratelimit.Limiter
+}
+
+func NewService(svc sms.Service, limiter ratelimit.Limiter) sms.Service {
+	return &Service{
+		svc:     svc,
+		limiter: limiter,
+	}
+}
+
+func (s *Service) Send(ctx context.Context, tpl string, args []string, numbers ...string) error {
+	key := fmt.Sprintf("sms:%s", s.key)
+	// 可以限流：保守策略，你的下游很坑的时候，
+	// 可以不限：你的下游很强，业务可用性要求很高，尽量容错策略
+	limited, err := s.limiter.Limit(ctx, key)
+	if err != nil {
+		// 系统错误,
+		// 保守策略，返回错误
+		// 非保守策略，跳过错误，直接发
+		// 包一下这个错误
+		return fmt.Errorf("短信服务限流出现问题，%w", err)
+	}
+	if limited {
+		return errLimited
+	}
+
+	// 你这里加一些代码，新特性
+	// err = s.svc.Send(ctx, tpl, args, numbers...)
+	// 你在这里也可以加一些代码，新特性
+	return s.svc.Send(ctx, tpl, args, numbers...)
+}
