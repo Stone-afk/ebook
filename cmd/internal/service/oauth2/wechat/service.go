b7b77bb74119b09000ebb4e2b676730a569c123c
Stone-afk <1711865140@qq.com>
2023-09-19 23:55:51 +0800

    feat: add user wechat

--- cmd/internal/service/oauth2/wechat/service.go
+++ cmd/internal/service/oauth2/wechat/service.go
@@ -1,10 +1,35 @@
 package wechat
 
-import "net/http"
+import (
+	"context"
+	"ebook/cmd/internal/domain"
+	"ebook/cmd/internal/service/oauth2"
+	"net/http"
+)
+
+var _ oauth2.Service = &Service{}
 
 type Service struct {
 	appId     string
 	appSecret string
 	client    *http.Client
 	//cmd       redis.Cmdable
+}
+
+type Result struct{}
+
+func NewService(appId string, appSecret string, client *http.Client) oauth2.Service {
+	return &Service{
+		appId:     appId,
+		appSecret: appSecret,
+		client:    client,
+	}
+}
+
+func (s *Service) AuthURL(ctx context.Context, state string) (string, error) {
+	panic("")
+}
+
+func (s *Service) VerifyCode(ctx context.Context, code string) (domain.WechatInfo, error) {
+	panic("")
 }
