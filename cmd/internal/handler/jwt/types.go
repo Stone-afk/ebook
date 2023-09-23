6bd29754d58f09832db6f36b8f8a94ef683fef0b
Stone-afk <1711865140@qq.com>
2023-09-24 00:24:19 +0800

    feat: add user wechat

--- cmd/internal/handler/jwt/types.go
+++ cmd/internal/handler/jwt/types.go
@@ -1,1 +1,25 @@
 package jwt
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Handler interface {
+	SetLoginToken(ctx *gin.Context, uid int64) error
+	SetJWTToken(ctx *gin.Context, uid int64, ssid string) error
+}
+
+type RefreshClaims struct {
+	UserId int64
+	Ssid   string
+	jwt.RegisteredClaims
+}
+
+type UserClaims struct {
+	jwt.RegisteredClaims
+	// 声明你自己的要放进去 token 里面的数据
+	UserId    int64
+	Ssid      string
+	UserAgent string
+}
