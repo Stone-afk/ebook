3eee2c0db0aacafdfd048c6adb7ab5312e3657a0
Stone-afk <1711865140@qq.com>
2023-09-27 22:44:55 +0800

    feat: add user jwt

--- cmd/internal/handler/jwt/types.go
+++ cmd/internal/handler/jwt/types.go
@@ -9,6 +9,8 @@ type Handler interface {
 	SetLoginToken(ctx *gin.Context, uid int64) error
 	SetJWTToken(ctx *gin.Context, uid int64, ssid string) error
 	ClearToken(ctx *gin.Context) error
+	CheckSession(ctx *gin.Context, ssid string) error
+	ExtractToken(ctx *gin.Context) string
 }
 
 type RefreshClaims struct {
