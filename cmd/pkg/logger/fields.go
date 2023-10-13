20ceb367a877afa05412a5401d241daceb6eab1d
Stone-afk <1711865140@qq.com>
2023-10-14 00:25:24 +0800

    feature: add log

---
+++ cmd/pkg/logger/fields.go
@@ -0,0 +1,27 @@
+package logger
+
+type Field struct {
+	Key   string
+	Value any
+}
+
+func String(key, val string) Field {
+	return Field{
+		Key:   key,
+		Value: val,
+	}
+}
+
+func Int64(key string, val int64) Field {
+	return Field{
+		Key:   key,
+		Value: val,
+	}
+}
+
+func Error(err error) Field {
+	return Field{
+		Key:   "error",
+		Value: err,
+	}
+}
