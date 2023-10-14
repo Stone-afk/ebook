bfc1b51aa075178b938f4c83562ea74735144a27
Stone-afk <1711865140@qq.com>
2023-10-14 11:56:59 +0800

    feature: add log

--- cmd/pkg/ginx/middleware/logger/builder.go
+++ cmd/pkg/ginx/middleware/logger/builder.go
@@ -1,10 +1,15 @@
 package logger
 
 import (
+	"bytes"
 	"context"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/atomic"
+	"io"
+	"time"
 )
+
+const printLimit = 1024
 
 // MiddlewareBuilder 注意点：
 // 1. 小心日志内容过多。URL 可能很长，请求体，响应体都可能很大，你要考虑是不是完全输出到日志里面
@@ -36,8 +41,50 @@ func (b *MiddlewareBuilder) AllowRespBody(ok bool) *MiddlewareBuilder {
 }
 
 func (b *MiddlewareBuilder) Build() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		url := ctx.Request.URL.String()
+		if len(url) > printLimit {
+			url = url[:1024]
+		}
+		accessLog := &AccessLog{
+			Method: ctx.Request.Method,
+			// URL 本身也可能很长
+			Url: url,
+		}
+		if b.allowReqBody.Load() && ctx.Request.Body != nil {
+			// Body 读完就没有了
+			body, _ := ctx.GetRawData()
+			// reader := io.NopCloser(bytes.NewBuffer(body))
+			reader := io.NopCloser(bytes.NewReader(body))
+			//ctx.Request.GetBody = func() (io.ReadCloser, error) {
+			//	return reader, nil
+			//}
+			ctx.Request.Body = reader
+			if len(body) > 1024 {
+				body = body[:1024]
+			}
+			// 这其实是一个很消耗 CPU 和内存的操作
+			// 因为会引起复制
+			accessLog.ReqBody = string(body)
+		}
 
+		if b.allowRespBody.Load() {
+			ctx.Writer = responseWriter{
+				accessLog:      accessLog,
+				ResponseWriter: ctx.Writer,
+			}
+		}
+
+		defer func() {
+			accessLog.Duration = time.Since(start).String()
+			// accessLog.Duration = time.Now().Sub(start).String()
+			b.loggerFunc(ctx, accessLog)
+		}()
+
+		// 执行到业务逻辑
+		ctx.Next()
+		//b.loggerFunc(ctx, al)
 	}
 }
 
@@ -50,4 +97,24 @@ type AccessLog struct {
 	ReqBody  string
 	RespBody string
 	Status   int
+}
+
+type responseWriter struct {
+	accessLog *AccessLog
+	gin.ResponseWriter
+}
+
+func (w responseWriter) WriteHeader(statusCode int) {
+	w.accessLog.Status = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w responseWriter) Write(data []byte) (int, error) {
+	w.accessLog.RespBody = string(data)
+	return w.ResponseWriter.Write(data)
+}
+
+func (w responseWriter) WriteString(data string) (int, error) {
+	w.accessLog.RespBody = data
+	return w.ResponseWriter.WriteString(data)
 }
