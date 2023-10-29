46c5ff186076ff9116fe9ef6cd1a49c5283fe9d2
Stone-afk <1711865140@qq.com>
2023-10-29 15:59:54 +0800

    art test

--- cmd/internal/repository/dao/init.go
+++ cmd/internal/repository/dao/init.go
@@ -1,10 +1,42 @@
 package dao
 
 import (
+	"context"
+	"ebook/cmd/internal/repository/dao/article"
 	"ebook/cmd/internal/repository/dao/user"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gorm.io/gorm"
+	"time"
 )
 
 func InitTables(db *gorm.DB) error {
-	return db.AutoMigrate(&user.User{})
+	return db.AutoMigrate(&user.User{}, &article.Article{},
+		&article.PublishedArticle{})
+}
+
+func InitCollections(db *mongo.Database) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	index := []mongo.IndexModel{
+		{
+			Keys:    bson.D{bson.E{Key: "id", Value: 1}},
+			Options: options.Index().SetUnique(true),
+		},
+		{
+			Keys: bson.D{bson.E{Key: "author_id", Value: 1},
+				bson.E{Key: "ctime", Value: 1},
+			},
+			Options: options.Index(),
+		},
+	}
+	_, err := db.Collection("articles").
+		Indexes().CreateMany(ctx, index)
+	if err != nil {
+		return err
+	}
+	_, err = db.Collection("published_articles").
+		Indexes().CreateMany(ctx, index)
+	return err
 }
