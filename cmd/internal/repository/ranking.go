889f10dda6eeb1bcc3c60199b3b646c97e960a20
Stone-afk <1711865140@qq.com>
2023-11-25 11:19:29 +0800

    feature: if

--- cmd/internal/repository/ranking.go
+++ cmd/internal/repository/ranking.go
@@ -13,20 +13,22 @@ type RankingRepository interface {
 }
 
 type rankingRepository struct {
+	localCache *cache.RankingLocalCache
 	redisCache *cache.RedisRankingCache
 	// 也可以考虑将这个本地缓存塞进去 RankingCache 里面，作为一个实现
 	topN atomicx.Value[[]domain.Article]
 }
 
-func NewCachedRankingRepository(redisCache *cache.RedisRankingCache) RankingRepository {
+func NewCachedRankingRepository(redisCache *cache.RedisRankingCache,
+	localCache *cache.RankingLocalCache) RankingRepository {
 	return &rankingRepository{
+		localCache: localCache,
 		redisCache: redisCache,
 	}
 }
 
 func (repo *rankingRepository) ReplaceTopN(ctx context.Context, arts []domain.Article) error {
-	//TODO implement me
-	panic("implement me")
+	return repo.redisCache.Set(ctx, arts)
 }
 
 func (repo *rankingRepository) GetTopN(ctx context.Context) ([]domain.Article, error) {
