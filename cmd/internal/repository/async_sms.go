ecc65c8ce6366eafff18662d80b0bd1935681723
Stone-afk <1711865140@qq.com>
2023-11-26 16:54:11 +0800

    feature: ast

--- cmd/internal/repository/async_sms.go
+++ cmd/internal/repository/async_sms.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"ebook/cmd/internal/domain"
 	"ebook/cmd/internal/repository/dao/async_sms"
+	"github.com/ecodeclub/ekit/sqlx"
 )
 
 var ErrWaitingSMSNotFound = async_sms.ErrWaitingSMSNotFound
@@ -28,16 +29,36 @@ func NewAsyncSMSRepository(dao async_sms.AsyncSmsDAO) AsyncSmsRepository {
 }
 
 func (repo *asyncSmsRepository) Add(ctx context.Context, s domain.AsyncSms) error {
-	//TODO implement me
-	panic("implement me")
+	return repo.dao.Insert(ctx, async_sms.AsyncSms{
+		RetryMax: s.RetryMax,
+		Config: sqlx.JsonColumn[async_sms.SmsConfig]{
+			Val: async_sms.SmsConfig{
+				TplId:   s.TplId,
+				Args:    s.Args,
+				Numbers: s.Numbers,
+			},
+			Valid: true,
+		},
+	})
 }
 
 func (repo *asyncSmsRepository) PreemptWaitingSMS(ctx context.Context) (domain.AsyncSms, error) {
-	//TODO implement me
-	panic("implement me")
+	as, err := repo.dao.GetWaitingSMS(ctx)
+	if err != nil {
+		return domain.AsyncSms{}, err
+	}
+	return domain.AsyncSms{
+		Id:       as.Id,
+		TplId:    as.Config.Val.TplId,
+		Numbers:  as.Config.Val.Numbers,
+		Args:     as.Config.Val.Args,
+		RetryMax: as.RetryMax,
+	}, nil
 }
 
 func (repo *asyncSmsRepository) ReportScheduleResult(ctx context.Context, id int64, success bool) error {
-	//TODO implement me
-	panic("implement me")
+	if success {
+		return repo.dao.MarkSuccess(ctx, id)
+	}
+	return repo.dao.MarkFailed(ctx, id)
 }
