4a0c4a20a542c055703fc26d7f7e671b55158ff8
Stone-afk <1711865140@qq.com>
2023-12-10 16:56:03 +0800

    feature: ranting

--- cmd/internal/domain/job.go
+++ cmd/internal/domain/job.go
@@ -1,11 +1,29 @@
 package domain
 
-import "time"
+import (
+	"github.com/robfig/cron/v3"
+	"time"
+)
 
 type CronJob struct {
 	Id int64
 	// Job 的名称，必须唯一
 	Name     string
-	Cfg      string
-	NextTime time.Time
+	Cron     string
+	Executor string
+	// 通用的任务的抽象，我们也不知道任务的具体细节，所以就搞一个 Cfg
+	// 具体任务设置具体的值
+	Cfg        string
+	CancelFunc func() error
+}
+
+var parser = cron.NewParser(cron.Minute | cron.Hour | cron.Dom |
+	cron.Month | cron.Dow | cron.Descriptor)
+
+func (j CronJob) NextTime() time.Time {
+	// 你怎么算？要根据 cron 表达式来算
+	// 可以做成包变量，因为基本不可能变
+
+	s, _ := parser.Parse(j.Cron)
+	return s.Next(time.Now())
 }
--- cmd/internal/service/cron_job.go
+++ cmd/internal/service/cron_job.go
@@ -12,6 +12,11 @@ var ErrNoMoreJob = repository.ErrNoMoreJob
 
 type CronJobService interface {
 	Preempt(ctx context.Context) (domain.CronJob, error)
+	ResetNextTime(ctx context.Context, j domain.CronJob) error
+	// 返回一个释放的方法，然后调用者取调
+	// PreemptV1(ctx context.Context) (domain.Job, func() error,  error)
+	// Release
+	//Release(ctx context.Context, id int64) error
 }
 
 type cronJobService struct {
@@ -35,5 +40,62 @@ func (s *cronJobService) Preempt(ctx context.Context) (domain.CronJob, error) {
 	if err != nil {
 		return domain.CronJob{}, err
 	}
+	// 你的续约呢？
+	//ch := make(chan struct{})
+	//go func() {
+	//	ticker := time.NewTicker(p.refreshInterval)
+	//	for {
+	//		select {
+	//		case <-ticker.C:
+	//			// 在这里续约
+	//			p.refresh(j.Id)
+	//		case <-ch:
+	//			// 结束
+	//			return
+	//		}
+	//	}
+	//}()
+	ticker := time.NewTicker(s.refreshInterval)
+	go func() {
+		for range ticker.C {
+			s.refresh(j.Id)
+		}
+	}()
+
+	// 你抢占之后，你一直抢占着吗？
+	// 你要考虑一个释放的问题
+
+	j.CancelFunc = func() error {
+		//close(ch)
+		// 自己在这里释放掉
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		return s.repo.Release(ctx, j.Id)
+	}
 	return j, nil
+}
+
+func (s *cronJobService) ResetNextTime(ctx context.Context, j domain.CronJob) error {
+	next := j.NextTime()
+	if next.IsZero() {
+		// 没有下一次
+		return s.repo.Stop(ctx, j.Id)
+	}
+	return s.repo.UpdateNextTime(ctx, j.Id, next)
+}
+
+func (s *cronJobService) refresh(id int64) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// 续约怎么个续法？
+	// 更新一下更新时间就可以
+	// 比如说我们的续约失败逻辑就是：处于 running 状态，
+	// 但是更新时间在三分钟以前, 代表发生该情况该通常是该节点挂掉了
+	err := s.repo.UpdateUtime(ctx, id)
+	if err != nil {
+		// 可以考虑立刻重试
+		s.l.Error("续约失败",
+			logger.Error(err),
+			logger.Int64("jid", id))
+	}
 }
