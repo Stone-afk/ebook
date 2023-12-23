7d2ceb0b609fb1de6f14515834be8b397a787ea1
Stone-afk <1711865140@qq.com>
2023-12-23 23:30:18 +0800

    feature add wrie

--- cmd/internal/service/oauth2/types.go
+++ cmd/internal/service/oauth2/types.go
@@ -5,7 +5,13 @@ import (
 	"ebook/cmd/internal/domain"
 )
 
+//go:generate mockgen -source=/Users/stone/go_project/ebook/ebook/cmd/internal/service/oauth2/types.go -package=oauth2mocks -destination=/Users/stone/go_project/ebook/ebook/cmd/internal/service/oauth2/mocks/svc.mock.go
 type Service interface {
 	AuthURL(ctx context.Context, state string) (string, error)
+	// VerifyCode 目前大部分公司的 OAuth2 平台都差不多的设计
+	// 返回一个 unionId。这个你可以理解为，在第三方平台上的 unionId
+	// 你也可以考虑使用 openId 来替换
+	// 一家公司如果有很多应用，不同应用都有自建的用户系统
+	// 那么 openId 可能更加合适
 	VerifyCode(ctx context.Context, code string) (domain.WechatInfo, error)
 }
