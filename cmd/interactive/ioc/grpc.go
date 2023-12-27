59aa39a04294ba5d58f75fdc21bf8db31a620f48
Stone-afk <1711865140@qq.com>
2023-12-27 20:05:20 +0800

    feature: add grpc

--- cmd/interactive/ioc/grpc.go
+++ cmd/interactive/ioc/grpc.go
@@ -3,8 +3,24 @@ package ioc
 import (
 	grpc2 "ebook/cmd/interactive/grpc"
 	"ebook/cmd/pkg/grpcx"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 func InitGRPCxServer(intrServer *grpc2.InteractiveServiceServer) *grpcx.Server {
-	panic("")
+	type Config struct {
+		Addr string `yaml:"addr"`
+	}
+	var cfg Config
+	err := viper.UnmarshalKey("grpc.server", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	server := grpc.NewServer()
+	intrServer.Register(server)
+
+	return &grpcx.Server{
+		Server: server,
+		Addr:   cfg.Addr,
+	}
 }
