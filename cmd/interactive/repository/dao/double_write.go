27b2d08a241ca2ab98a1d10cc2dbad40f378172d
Stone-afk <1711865140@qq.com>
2024-01-13 10:47:37 +0800

    feature conn pool

--- cmd/interactive/repository/dao/double_write.go
+++ cmd/interactive/repository/dao/double_write.go
@@ -1,6 +1,7 @@
 package dao
 
 import (
+	"context"
 	"github.com/ecodeclub/ekit/syncx/atomicx"
 	"gorm.io/gorm"
 )
@@ -32,6 +33,46 @@ func NewDoubleWriteDAOV1(src *gorm.DB, dst *gorm.DB) *DoubleWriteDAO {
 		dst:     NewGORMInteractiveDAO(dst),
 		pattern: atomicx.NewValueOf(patternSrcOnly),
 	}
+}
+
+func (d *DoubleWriteDAO) InsertLikeInfo(ctx context.Context, biz string, bizId, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) GetLikeInfo(ctx context.Context, biz string, bizId, uid int64) (UserLikeBiz, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) DeleteLikeInfo(ctx context.Context, biz string, bizId, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) Get(ctx context.Context, biz string, bizId int64) (Interactive, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) InsertCollectionBiz(ctx context.Context, cb UserCollectionBiz) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) GetCollectionInfo(ctx context.Context, biz string, bizId, uid int64) (UserCollectionBiz, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) BatchIncrReadCnt(ctx context.Context, bizs []string, ids []int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DoubleWriteDAO) GetByIds(ctx context.Context, biz string, ids []int64) ([]Interactive, error) {
+	//TODO implement me
+	panic("implement me")
 }
 
 func (d *DoubleWriteDAO) UpdatePattern(pattern string) {
