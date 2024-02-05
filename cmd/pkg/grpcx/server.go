360752bea1a05ff057b0ff718cced64da9ccf019
Stone-afk <1711865140@qq.com>
2024-02-05 23:04:53 +0800

    feature：etdcd

--- cmd/pkg/grpcx/server.go
+++ cmd/pkg/grpcx/server.go
@@ -1,20 +1,41 @@
 package grpcx
 
 import (
+	"ebook/cmd/pkg/logger"
+	etcdv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/endpoints"
 	"google.golang.org/grpc"
 	"net"
+	"strconv"
 )
 
 type Server struct {
 	*grpc.Server
-	Addr string
+	//Addr string
+	L      logger.Logger
+	em     endpoints.Manager
+	client *etcdv3.Client
+
+	Port      int
+	EtcdAddrs []string
+	Name      string
+	kaCancel  func()
 }
 
 func (s *Server) Serve() error {
-	l, err := net.Listen("tcp", ":8090")
+	l, err := net.Listen("tcp", ":"+strconv.Itoa(s.Port))
 	if err != nil {
 		return err
 	}
 	// 这边会阻塞，类似与 gin.Run
 	return s.Server.Serve(l)
+}
+
+func (s *Server) register() error {
+	panic("")
+}
+
+// Close 你可以叫做 Shutdown
+func (s *Server) Close() error {
+	panic("")
 }
