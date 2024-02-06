b34ac5580e1fa628dad95376195664e7c046f2a6
Stone-afk <1711865140@qq.com>
2024-02-06 11:36:56 +0800

    feature：register

--- cmd/pkg/grpcx/server.go
+++ cmd/pkg/grpcx/server.go
@@ -3,6 +3,7 @@ package grpcx
 import (
 	"context"
 	"ebook/cmd/pkg/logger"
+	"ebook/cmd/pkg/netx"
 	etcdv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/client/v3/naming/endpoints"
 	"google.golang.org/grpc"
@@ -35,7 +36,43 @@ func (s *Server) Serve() error {
 }
 
 func (s *Server) register() error {
-	panic("")
+	client, err := etcdv3.New(etcdv3.Config{
+		Endpoints: s.EtcdAddrs,
+	})
+	if err != nil {
+		return err
+	}
+	s.client = client
+	// endpoint 以服务为维度。一个服务一个 Manager
+	em, err := endpoints.NewManager(client, "service/"+s.Name)
+	if err != nil {
+		return err
+	}
+
+	addr := netx.GetOutboundIP() + ":" + strconv.Itoa(s.Port)
+	key := "service/" + s.Name + "/" + addr
+	s.key = key
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// 你可以做成配置的
+	var ttl int64 = 30
+	leaseResp, err := client.Grant(ctx, ttl)
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err = em.AddEndpoint(ctx, key, endpoints.Endpoint{
+		Addr: addr,
+	}, etcdv3.WithLease(leaseResp.ID))
+
+	kaCtx, kaCancel := context.WithCancel(context.Background())
+	s.kaCancel = kaCancel
+	ch, err := s.client.KeepAlive(kaCtx, leaseResp.ID)
+	go func() {
+		for kaResp := range ch {
+			// 正常就是打印一下 DEBUG 日志啥的
+			s.L.Debug(kaResp.String())
+		}
+	}()
+	return nil
 }
 
 // Close 你可以叫做 Shutdown
