fd086c546259812b92e1250616a4f8404a774668
Stone-afk <1711865140@qq.com>
2024-02-20 20:22:48 +0800

    feature：add ratelimit

--- cmd/pkg/ratelimit/leaky_bucket.go
+++ cmd/pkg/ratelimit/leaky_bucket.go
@@ -1,6 +1,7 @@
 package ratelimit
 
 import (
+	"context"
 	"sync"
 	"time"
 )
@@ -12,4 +13,15 @@ type LeakyBucket struct {
 
 	closeCh   chan struct{}
 	closeOnce sync.Once
+}
+
+func (l *LeakyBucket) Limit(ctx context.Context, key string) (bool, error) {
+	panic("")
+}
+
+func (l *LeakyBucket) Close() error {
+	l.closeOnce.Do(func() {
+		close(l.closeCh)
+	})
+	return nil
 }
