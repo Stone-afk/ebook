bad80fd74ff1b1e6d61f87fd639a20d5d8f78db6
Stone-afk <1711865140@qq.com>
2024-03-06 20:50:04 +0800

    feature：add payment

--- cmd/payment/repository/dao/gorm.go
+++ cmd/payment/repository/dao/gorm.go
@@ -11,14 +11,14 @@ type PaymentGORMDAO struct {
 	db *gorm.DB
 }
 
-func (d PaymentGORMDAO) Insert(ctx context.Context, pmt Payment) error {
+func (d *PaymentGORMDAO) Insert(ctx context.Context, pmt Payment) error {
 	now := time.Now().UnixMilli()
 	pmt.Utime = now
 	pmt.Ctime = now
 	return d.db.WithContext(ctx).Create(&pmt).Error
 }
 
-func (d PaymentGORMDAO) UpdateTxnIDAndStatus(ctx context.Context, bizTradeNo string, txnID string, status domain.PaymentStatus) error {
+func (d *PaymentGORMDAO) UpdateTxnIDAndStatus(ctx context.Context, bizTradeNo string, txnID string, status domain.PaymentStatus) error {
 	return d.db.WithContext(ctx).Model(&Payment{}).
 		Where("biz_trade_no = ?", bizTradeNo).
 		Updates(map[string]any{
@@ -28,14 +28,14 @@ func (d PaymentGORMDAO) UpdateTxnIDAndStatus(ctx context.Context, bizTradeNo str
 		}).Error
 }
 
-func (d PaymentGORMDAO) FindExpiredPayment(ctx context.Context, offset int, limit int, t time.Time) ([]Payment, error) {
+func (d *PaymentGORMDAO) FindExpiredPayment(ctx context.Context, offset int, limit int, t time.Time) ([]Payment, error) {
 	var res []Payment
 	err := d.db.WithContext(ctx).Where("status = ? AND utime < ?",
 		uint8(domain.PaymentStatusInit), t.UnixMilli()).Offset(offset).Limit(limit).Find(&res).Error
 	return res, err
 }
 
-func (d PaymentGORMDAO) GetPayment(ctx context.Context, bizTradeNO string) (Payment, error) {
+func (d *PaymentGORMDAO) GetPayment(ctx context.Context, bizTradeNO string) (Payment, error) {
 	var res Payment
 	err := d.db.WithContext(ctx).Where("biz_trade_no = ?", bizTradeNO).First(&res).Error
 	return res, err
