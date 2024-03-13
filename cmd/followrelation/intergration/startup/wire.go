369e17ea63d6aeb104bfef8b11637eade3f5153c
Stone-afk <1711865140@qq.com>
2024-03-13 13:52:46 +0800

    feature：add follow

--- cmd/followrelation/intergration/startup/wire.go
+++ cmd/followrelation/intergration/startup/wire.go
@@ -1,1 +1,26 @@
+//go:build wireinject
 package startup
+
+import (
+	grpc2 "ebook/cmd/followrelation/grpc"
+	"ebook/cmd/followrelation/repository"
+	"ebook/cmd/followrelation/repository/cache"
+	"ebook/cmd/followrelation/repository/dao"
+	"ebook/cmd/followrelation/service"
+	"github.com/google/wire"
+)
+
+//go:generate wire
+func InitServer() *grpc2.FollowServiceServer {
+	wire.Build(
+		InitRedis,
+		InitLogger,
+		InitTestDB,
+		dao.NewGORMFollowRelationDAO,
+		cache.NewRedisFollowCache,
+		repository.NewFollowRelationRepository,
+		service.NewFollowRelationService,
+		grpc2.NewFollowRelationServiceServer,
+	)
+	return new(grpc2.FollowServiceServer)
+}
