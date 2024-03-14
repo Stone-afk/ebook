914480a683fc211cd95006e5ed582e8d5c0faaa0
Stone-afk <1711865140@qq.com>
2024-03-14 10:58:25 +0800

    feature：add search

--- cmd/search/repository/user.go
+++ cmd/search/repository/user.go
@@ -4,20 +4,35 @@ import (
 	"context"
 	"ebook/cmd/search/domain"
 	"ebook/cmd/search/repository/dao"
+	"github.com/ecodeclub/ekit/slice"
 )
 
 type userRepository struct {
 	dao dao.UserDAO
 }
 
-func (dao *userRepository) InputUser(ctx context.Context, msg domain.User) error {
-	//TODO implement me
-	panic("implement me")
+func (repo *userRepository) InputUser(ctx context.Context, msg domain.User) error {
+	return repo.dao.InputUser(ctx, dao.User{
+		Id:       msg.Id,
+		Email:    msg.Email,
+		Nickname: msg.Nickname,
+		Phone:    msg.Phone,
+	})
 }
 
-func (dao *userRepository) SearchUser(ctx context.Context, keywords []string) ([]domain.User, error) {
-	//TODO implement me
-	panic("implement me")
+func (repo *userRepository) SearchUser(ctx context.Context, keywords []string) ([]domain.User, error) {
+	users, err := repo.dao.Search(ctx, keywords)
+	if err != nil {
+		return nil, err
+	}
+	return slice.Map(users, func(idx int, src dao.User) domain.User {
+		return domain.User{
+			Id:       src.Id,
+			Email:    src.Email,
+			Nickname: src.Nickname,
+			Phone:    src.Phone,
+		}
+	}), nil
 }
 
 func NewUserRepository(d dao.UserDAO) UserRepository {
--- cmd/search/service/types.go
+++ cmd/search/service/types.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"context"
+	"ebook/cmd/search/domain"
+)
+
+type SearchService interface {
+	Search(ctx context.Context, uid int64, expression string) (domain.SearchResult, error)
+}
+
+type ArticleSearchService interface {
+	SearchArticle(ctx context.Context, uid int64, expression string) (domain.SearchResult, error)
+}
+
+type UserSearchService interface {
+	SearchUser(ctx context.Context, expression string) (domain.SearchResult, error)
+}
+
+type SyncService interface {
+	InputArticle(ctx context.Context, article domain.Article) error
+	InputUser(ctx context.Context, user domain.User) error
+	InputAny(ctx context.Context, index, docID, data string) error
+}
