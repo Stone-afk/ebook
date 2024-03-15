0ec81bf4b28cb944e9baffcd3a65b317245a4409
Stone-afk <1711865140@qq.com>
2024-03-15 14:44:33 +0800

    feature：add tag

--- cmd/tag/repository/cache/redis.go
+++ cmd/tag/repository/cache/redis.go
@@ -3,7 +3,10 @@ package cache
 import (
 	"context"
 	"ebook/cmd/tag/domain"
+	"encoding/json"
+	"fmt"
 	"github.com/redis/go-redis/v9"
+	"strconv"
 	"time"
 )
 
@@ -13,18 +16,66 @@ type RedisTagCache struct {
 }
 
 func (r *RedisTagCache) GetTags(ctx context.Context, uid int64) ([]domain.Tag, error) {
-	//TODO implement me
-	panic("implement me")
+	key := r.userTagsKey(uid)
+	//data, err := r.client.LRange(ctx, key, 0, -1).Result()
+	//if err != nil {
+	//	return nil, err
+	//}
+	//res := make([]domain.Tag, 0, len(data))
+	//for _, ele := range data {
+	//	var t domain.Tag
+	//	err = json.Unmarshal([]byte(ele), &t)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	res = append(res, t)
+	//}
+
+	data, err := r.client.HGetAll(ctx, key).Result()
+	if err != nil {
+		return nil, err
+	}
+	res := make([]domain.Tag, 0, len(data))
+	for _, ele := range data {
+		var t domain.Tag
+		err = json.Unmarshal([]byte(ele), &t)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, t)
+	}
+	return res, nil
 }
 
 func (r *RedisTagCache) Append(ctx context.Context, uid int64, tags ...domain.Tag) error {
-	//TODO implement me
-	panic("implement me")
+	// data := make([]any, 0, len(tags))
+	key := r.userTagsKey(uid)
+	pip := r.client.Pipeline()
+	for _, tag := range tags {
+		val, err := json.Marshal(tag)
+		if err != nil {
+			return err
+		}
+		// data = append(data, val)
+		pip.HMSet(ctx, key, strconv.FormatInt(tag.Id, 10), val)
+	}
+	// 利用 pipeline 来执行，性能好一点
+
+	// pip.RPush(ctx, key, data...) 使用 List 在多个实例启动时预加载数据可能导致冲突，所以改成 hash
+	// 无法辨别 key 是不是已经有过期时间，
+	// 如果这个 key 的第一个 tag，是没有过期时间
+	// 可以不设置过期时间
+	pip.Expire(ctx, key, r.expiration)
+	_, err := pip.Exec(ctx)
+	return err
 }
 
 func (r *RedisTagCache) DelTags(ctx context.Context, uid int64) error {
-	//TODO implement me
-	panic("implement me")
+	return r.client.Del(ctx, r.userTagsKey(uid)).Err()
+}
+
+func (r *RedisTagCache) userTagsKey(uid int64) string {
+	return fmt.Sprintf("tag:user_tags:%d", uid)
 }
 
 func NewRedisTagCache(client redis.Cmdable) TagCache {
--- cmd/tag/repository/tag.go
+++ cmd/tag/repository/tag.go
@@ -14,6 +14,26 @@ type CachedTagRepository struct {
 	l     logger.Logger
 }
 
+// PreloadUserTags 在 toB 的场景下，你可以提前预加载缓存
+func (repo *CachedTagRepository) PreloadUserTags(ctx context.Context) error {
+	// 怎么预加载？
+	// 缓存里面，究竟怎么存？
+	// 1. 放 json，json 里面是一个用户的所有的标签 uid => [{}, {}]
+	// 按照用户 ID 来查找
+	//var uid int64= 1
+	//for {
+	//	repo.dao.GetTagsByUid(ctx, uid)
+	//	uid ++
+	//}
+	// select * from tags group by uid
+	// 使用 redis 的数据结构
+	// 1. list  使用 List 在多个实例启动时预加载数据可能导致冲突，所以改成 hash
+	// 2. hash 用 hash 结构
+	// 3. set, sorted set 都可以
+	//TODO implement me
+	panic("implement me")
+}
+
 func (repo *CachedTagRepository) CreateTag(ctx context.Context, tag domain.Tag) (int64, error) {
 	//TODO implement me
 	panic("implement me")
