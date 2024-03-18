c6238e31fd3491a8b17969658aec158bf26a2b2d
Stone-afk <1711865140@qq.com>
2024-03-18 21:18:19 +0800

    feature：add search

--- cmd/search/ioc/kafka.go
+++ cmd/search/ioc/kafka.go
@@ -1,1 +1,33 @@
 package ioc
+
+import (
+	"ebook/cmd/pkg/saramax"
+	"ebook/cmd/search/events"
+	"github.com/IBM/sarama"
+	"github.com/spf13/viper"
+)
+
+func InitKafka() sarama.Client {
+	type Config struct {
+		Addrs []string `yaml:"addrs"`
+	}
+	saramaCfg := sarama.NewConfig()
+	saramaCfg.Producer.Return.Successes = true
+	saramaCfg.Producer.Partitioner = sarama.NewConsistentCRCHashPartitioner
+	var cfg Config
+	err := viper.UnmarshalKey("kafka", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	client, err := sarama.NewClient(cfg.Addrs, saramaCfg)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func NewConsumers(articleConsumer *events.ArticleConsumer,
+	tagConsumer *events.TagConsumer,
+	userConsumer *events.UserConsumer, syncDataEventConsumer *events.SyncDataEventConsumer) []saramax.Consumer {
+	return []saramax.Consumer{articleConsumer, tagConsumer, userConsumer, syncDataEventConsumer}
+}
