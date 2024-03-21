3c56e340847d641dbafd54dc7dece8fc3dfd0052
Stone-afk <1711865140@qq.com>
2024-03-21 15:18:59 +0800

    feature: add producer

--- cmd/search/grpc/search.go
+++ cmd/search/grpc/search.go
@@ -1,1 +1,97 @@
 package grpc
+
+import (
+	"context"
+	searchv1 "ebook/cmd/api/proto/gen/search/v1"
+	"ebook/cmd/search/domain"
+	"ebook/cmd/search/service"
+	"github.com/ecodeclub/ekit/slice"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type SearchServiceServer struct {
+	searchv1.UnimplementedSearchServiceServer
+	searchv1.UnimplementedArticleSearchServiceServer
+	searchv1.UnimplementedTagSearchServiceServer
+	searchv1.UnimplementedUserSearchServiceServer
+	searchService  service.SearchService
+	userService    service.UserSearchService
+	articleService service.ArticleSearchService
+	tagService     service.TagService
+}
+
+func (s *SearchServiceServer) SearchUser(ctx context.Context, request *searchv1.UserSearchRequest) (*searchv1.UserSearchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SearchUser not implemented")
+}
+
+func (s *SearchServiceServer) SearchBizTags(ctx context.Context, request *searchv1.BizTagsSearchRequest) (*searchv1.BizTagsSearchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SearchBizTags not implemented")
+}
+
+func (s *SearchServiceServer) SearchArticle(ctx context.Context, request *searchv1.ArticleSearchRequest) (*searchv1.ArticleSearchResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SearchArticle not implemented")
+}
+
+func (s *SearchServiceServer) Search(ctx context.Context, request *searchv1.SearchRequest) (*searchv1.SearchResponse, error) {
+	resp, err := s.searchService.Search(ctx, request.Uid, request.Expression)
+	if err != nil {
+		return nil, err
+	}
+	return &searchv1.SearchResponse{
+		User: &searchv1.UserResult{
+			Users: slice.Map(resp.Users, func(idx int, src domain.User) *searchv1.User {
+				return userConvertToView(src)
+			}),
+		},
+		Article: &searchv1.ArticleResult{
+			Articles: slice.Map(resp.Articles, func(idx int, src domain.Article) *searchv1.Article {
+				return articleConvertToView(src)
+			}),
+		},
+	}, nil
+}
+
+func NewSearchService(searchService service.SearchService,
+	userService service.UserSearchService,
+	articleService service.ArticleSearchService,
+	tagService service.TagService) *SearchServiceServer {
+	return &SearchServiceServer{
+		searchService:  searchService,
+		userService:    userService,
+		articleService: articleService,
+		tagService:     tagService,
+	}
+}
+
+func (s *SearchServiceServer) Register(server grpc.ServiceRegistrar) {
+	searchv1.RegisterSearchServiceServer(server, s)
+}
+
+func bizTagsConvertToView(bizTags domain.BizTags) *searchv1.BizTags {
+	return &searchv1.BizTags{
+		Uid:   bizTags.Uid,
+		Biz:   bizTags.Biz,
+		Bizid: bizTags.BizId,
+		Tags:  bizTags.Tags,
+	}
+}
+
+func articleConvertToView(art domain.Article) *searchv1.Article {
+	return &searchv1.Article{
+		Id:      art.Id,
+		Title:   art.Title,
+		Status:  art.Status,
+		Content: art.Content,
+	}
+}
+
+func userConvertToView(u domain.User) *searchv1.User {
+	return &searchv1.User{
+		Id:       u.Id,
+		Nickname: u.Nickname,
+		Email:    u.Email,
+		Phone:    u.Phone,
+	}
+}
--- cmd/search/grpc/sync.go
+++ cmd/search/grpc/sync.go
@@ -1,1 +1,72 @@
 package grpc
+
+import (
+	"context"
+	searchv1 "ebook/cmd/api/proto/gen/search/v1"
+	"ebook/cmd/search/domain"
+	"ebook/cmd/search/service"
+	"google.golang.org/grpc"
+)
+
+type SyncServiceServer struct {
+	searchv1.UnimplementedSyncServiceServer
+	syncSvc service.SyncService
+}
+
+func (s *SyncServiceServer) InputBizTags(ctx context.Context, request *searchv1.InputBizTagsRequest) (*searchv1.InputBizTagsResponse, error) {
+	err := s.syncSvc.InputBizTags(ctx, toDomainBizTags(request.GetBiztags()))
+	return &searchv1.InputBizTagsResponse{}, err
+}
+
+// InputUser 业务专属接口，你可以高度定制化
+func (s *SyncServiceServer) InputUser(ctx context.Context, request *searchv1.InputUserRequest) (*searchv1.InputUserResponse, error) {
+	err := s.syncSvc.InputUser(ctx, toDomainUser(request.GetUser()))
+	return &searchv1.InputUserResponse{}, err
+}
+
+func (s *SyncServiceServer) InputArticle(ctx context.Context, request *searchv1.InputArticleRequest) (*searchv1.InputArticleResponse, error) {
+	err := s.syncSvc.InputArticle(ctx, toDomainArticle(request.GetArticle()))
+	return &searchv1.InputArticleResponse{}, err
+}
+
+func (s *SyncServiceServer) InputAny(ctx context.Context, req *searchv1.InputAnyRequest) (*searchv1.InputAnyResponse, error) {
+	err := s.syncSvc.InputAny(ctx, req.IndexName, req.DocId, req.Data)
+	return &searchv1.InputAnyResponse{}, err
+}
+
+func NewSyncServiceServer(syncSvc service.SyncService) *SyncServiceServer {
+	return &SyncServiceServer{
+		syncSvc: syncSvc,
+	}
+}
+
+func (s *SyncServiceServer) Register(server grpc.ServiceRegistrar) {
+	searchv1.RegisterSyncServiceServer(server, s)
+}
+
+func toDomainUser(vuser *searchv1.User) domain.User {
+	return domain.User{
+		Id:       vuser.Id,
+		Email:    vuser.Email,
+		Nickname: vuser.Nickname,
+	}
+}
+
+func toDomainArticle(art *searchv1.Article) domain.Article {
+	return domain.Article{
+		Id:      art.Id,
+		Title:   art.Title,
+		Status:  art.Status,
+		Content: art.Content,
+		Tags:    art.Tags,
+	}
+}
+
+func toDomainBizTags(bizTags *searchv1.BizTags) domain.BizTags {
+	return domain.BizTags{
+		Uid:   bizTags.Uid,
+		Biz:   bizTags.Biz,
+		BizId: bizTags.Bizid,
+		Tags:  bizTags.Tags,
+	}
+}
