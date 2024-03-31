e1c08a52ba824c42df9e0f182691dd6af061bd4c
Stone-afk <1711865140@qq.com>
2024-03-31 13:48:16 +0800

    feature: add feed

--- cmd/feed/events/article.go
+++ cmd/feed/events/article.go
@@ -1,1 +1,57 @@
 package events
+
+import (
+	"context"
+	"ebook/cmd/feed/domain"
+	"ebook/cmd/feed/service"
+	"ebook/cmd/pkg/logger"
+	"ebook/cmd/pkg/saramax"
+	"github.com/IBM/sarama"
+	"strconv"
+	"time"
+)
+
+const topicArticleEvent = "article_feed_event"
+
+// ArticleFeedEvent 由业务方定义，本服务做适配
+type ArticleFeedEvent struct {
+	uid int64
+	aid int64
+}
+
+type ArticleEventConsumer struct {
+	client sarama.Client
+	l      logger.Logger
+	svc    service.FeedService
+}
+
+// Start 这边就是自己启动 goroutine 了
+func (c *ArticleEventConsumer) Start() error {
+	cg, err := sarama.NewConsumerGroupFromClient("articleFeed",
+		c.client)
+	if err != nil {
+		return err
+	}
+	go func() {
+		err := cg.Consume(context.Background(),
+			[]string{topicArticleEvent},
+			saramax.NewHandler[ArticleFeedEvent](c.l, c.Consume))
+		if err != nil {
+			c.l.Error("退出了消费循环异常", logger.Error(err))
+		}
+	}()
+	return err
+}
+func (c *ArticleEventConsumer) Consume(msg *sarama.ConsumerMessage,
+	evt ArticleFeedEvent) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	return c.svc.CreateFeedEvent(ctx, domain.FeedEvent{
+		Type: service.FollowEventName,
+		Ext: map[string]string{
+			"uid": strconv.FormatInt(evt.uid, 10),
+			"aid": strconv.FormatInt(evt.uid, 10),
+		},
+	})
+
+}
--- cmd/feed/repository/dao/feed.go
+++ cmd/feed/repository/dao/feed.go
@@ -10,18 +10,30 @@ type feedPullEventDAO struct {
 }
 
 func (dao *feedPullEventDAO) CreatePullEvent(ctx context.Context, event FeedPullEvent) error {
-	//TODO implement me
-	panic("implement me")
+	return dao.db.WithContext(ctx).Create(&event).Error
 }
 
-func (dao *feedPullEventDAO) FindPullEventList(ctx context.Context, uids []int64, timestamp, limit int64) ([]FeedPullEvent, error) {
-	//TODO implement me
-	panic("implement me")
+func (dao *feedPullEventDAO) FindPullEvents(ctx context.Context, uids []int64, timestamp, limit int64) ([]FeedPullEvent, error) {
+	var events []FeedPullEvent
+	err := dao.db.WithContext(ctx).
+		Where("uid in ?", uids).
+		Where("ctime < ?", timestamp).
+		Order("ctime desc").
+		Limit(int(limit)).
+		Find(&events).Error
+	return events, err
 }
 
-func (dao *feedPullEventDAO) FindPullEventListWithTyp(ctx context.Context, typ string, uids []int64, timestamp, limit int64) ([]FeedPullEvent, error) {
-	//TODO implement me
-	panic("implement me")
+func (dao *feedPullEventDAO) FindPullEventsWithTyp(ctx context.Context, typ string, uids []int64, timestamp, limit int64) ([]FeedPullEvent, error) {
+	var events []FeedPullEvent
+	err := dao.db.WithContext(ctx).
+		Where("uid in ?", uids).
+		Where("ctime < ?", timestamp).
+		Where("type = ?", typ).
+		Order("ctime desc").
+		Limit(int(limit)).
+		Find(&events).Error
+	return events, err
 }
 
 func NewFeedPullEventDAO(db *gorm.DB) FeedPullEventDAO {
@@ -35,18 +47,30 @@ type feedPushEventDAO struct {
 }
 
 func (dao *feedPushEventDAO) CreatePushEvents(ctx context.Context, events []FeedPushEvent) error {
-	//TODO implement me
-	panic("implement me")
+	return dao.db.WithContext(ctx).Create(events).Error
 }
 
-func (dao *feedPushEventDAO) GetPushEvents(ctx context.Context, uid int64, timestamp, limit int64) ([]FeedPushEvent, error) {
-	//TODO implement me
-	panic("implement me")
+func (dao *feedPushEventDAO) FindPushEvents(ctx context.Context, uid int64, timestamp, limit int64) ([]FeedPushEvent, error) {
+	var events []FeedPushEvent
+	err := dao.db.WithContext(ctx).
+		Where("uid = ?", uid).
+		Where("ctime < ?", timestamp).
+		Order("ctime desc").
+		Limit(int(limit)).
+		Find(&events).Error
+	return events, err
 }
 
-func (dao *feedPushEventDAO) GetPushEventsWithTyp(ctx context.Context, typ string, uid int64, timestamp, limit int64) ([]FeedPushEvent, error) {
-	//TODO implement me
-	panic("implement me")
+func (dao *feedPushEventDAO) FindPushEventsWithTyp(ctx context.Context, typ string, uid int64, timestamp, limit int64) ([]FeedPushEvent, error) {
+	var events []FeedPushEvent
+	err := dao.db.WithContext(ctx).
+		Where("uid = ?", uid).
+		Where("ctime < ?", timestamp).
+		Where("type = ?", typ).
+		Order("ctime desc").
+		Limit(int(limit)).
+		Find(&events).Error
+	return events, err
 }
 
 func NewFeedPushEventDAO(db *gorm.DB) FeedPushEventDAO {
