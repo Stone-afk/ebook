c0465835b1744dd5bc38d27dd1a65370a983fec8
Stone-afk <1711865140@qq.com>
2024-04-02 19:53:45 +0800

    feature: add feed

--- cmd/feed/service/like_event.go
+++ cmd/feed/service/like_event.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"ebook/cmd/feed/domain"
 	"ebook/cmd/feed/repository"
+	"time"
 )
 
 const (
@@ -14,14 +15,30 @@ type LikeEventHandler struct {
 	repo repository.FeedEventRepo
 }
 
+// CreateFeedEvent 中的 ext 里面至少需要三个 id
+// liked int64: 被点赞的人
+// liker int64：点赞的人
+// bizId int64: 被点赞的东西
+// biz: string
 func (h *LikeEventHandler) CreateFeedEvent(ctx context.Context, ext domain.ExtendFields) error {
-	//TODO implement me
-	panic("implement me")
+	liked, err := ext.Get("liked").AsInt64()
+	if err != nil {
+		return err
+	}
+	// 你可以考虑校验其它数据
+	// 如果你用的是扩展表设计，那么这里就会调用自己业务的扩展表来存储数据
+	// 如果你希望冗余存储数据，但是业务方又不愿意存，
+	// 那么你在这里可以考虑回查业务获得一些数据
+	return h.repo.CreatePushEvents(ctx, []domain.FeedEvent{{
+		Uid:   liked,
+		Type:  LikeEventName,
+		Ctime: time.Now(),
+		Ext:   ext,
+	}})
 }
 
 func (h *LikeEventHandler) FindFeedEvents(ctx context.Context, uid, timestamp, limit int64) ([]domain.FeedEvent, error) {
-	//TODO implement me
-	panic("implement me")
+	return h.repo.FindPushEventsWithTyp(ctx, LikeEventName, uid, timestamp, limit)
 }
 
 func NewLikeEventHandler(repo repository.FeedEventRepo) Handler {
