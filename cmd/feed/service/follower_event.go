37acac27cb33200cd170b20a6ce2f693ef51e901
Stone-afk <1711865140@qq.com>
2024-04-02 13:26:07 +0800

    feature: add feed

--- cmd/feed/service/follower_event.go
+++ cmd/feed/service/follower_event.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"ebook/cmd/feed/domain"
 	"ebook/cmd/feed/repository"
+	"time"
 )
 
 const (
@@ -14,14 +15,25 @@ type FollowEventHandler struct {
 	repo repository.FeedEventRepo
 }
 
+// CreateFeedEvent 创建跟随方式
+// 如果 A 关注了 B，那么
+// follower 就是 A
+// followee 就是 B
 func (h *FollowEventHandler) CreateFeedEvent(ctx context.Context, ext domain.ExtendFields) error {
-	//TODO implement me
-	panic("implement me")
+	followee, err := ext.Get("followee").AsInt64()
+	if err != nil {
+		return err
+	}
+	return h.repo.CreatePushEvents(ctx, []domain.FeedEvent{{
+		Uid:   followee,
+		Type:  FollowEventName,
+		Ctime: time.Now(),
+		Ext:   ext,
+	}})
 }
 
 func (h *FollowEventHandler) FindFeedEvents(ctx context.Context, uid, timestamp, limit int64) ([]domain.FeedEvent, error) {
-	//TODO implement me
-	panic("implement me")
+	return h.repo.FindPushEventsWithTyp(ctx, FollowEventName, uid, timestamp, limit)
 }
 
 func NewFollowEventHandler(repo repository.FeedEventRepo) Handler {
