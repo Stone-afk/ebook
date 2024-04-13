150c909ce18b96f9cbf14e50e0017559392efcad
Stone-afk <1711865140@qq.com>
2024-04-13 20:46:14 +0800

    feature: add im

--- cmd/im/events/mysql_binlog_event.go
+++ cmd/im/events/mysql_binlog_event.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"ebook/cmd/im/service"
+	"ebook/cmd/pkg/canalx"
 	"ebook/cmd/pkg/logger"
 	"github.com/IBM/sarama"
 )
@@ -10,4 +11,39 @@ type MySQLBinlogConsumer struct {
 	client sarama.Client
 	l      logger.Logger
 	svc    service.UserService
+}
+
+func NewMySQLBinlogConsumer(client sarama.Client,
+	l logger.Logger, svc service.UserService) *MySQLBinlogConsumer {
+	return &MySQLBinlogConsumer{
+		client: client,
+		l:      l,
+		svc:    svc,
+	}
+}
+
+func (r *MySQLBinlogConsumer) Start() error {
+	panic("")
+}
+
+func (r *MySQLBinlogConsumer) Consume(msg *sarama.ConsumerMessage,
+	val canalx.Message[User]) error {
+	panic("")
+}
+
+type User struct {
+	Id            int64
+	Email         string
+	Password      string
+	Phone         string
+	Birthday      string
+	Nickname      string
+	AboutMe       string
+	WechatOpenId  string
+	WechatUnionId string
+
+	// 创建时间
+	Ctime int64
+	// 更新时间
+	Utime int64
 }
