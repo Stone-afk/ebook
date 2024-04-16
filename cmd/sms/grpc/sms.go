4e1d1551ad439cc438f638045a99b74ec5021222
Stone-afk <1711865140@qq.com>
2024-04-16 09:48:41 +0800

    feature: add sms

--- cmd/sms/grpc/sms.go
+++ cmd/sms/grpc/sms.go
@@ -1,1 +1,28 @@
 package grpc
+
+import (
+	"context"
+	smsv1 "ebook/cmd/api/proto/gen/sms/v1"
+	"ebook/cmd/sms/service"
+	"google.golang.org/grpc"
+)
+
+type SmsServiceServer struct {
+	smsv1.UnimplementedSmsServiceServer
+	svc service.Service
+}
+
+func NewSmsServiceServer(svc service.Service) *SmsServiceServer {
+	return &SmsServiceServer{
+		svc: svc,
+	}
+}
+
+func (s *SmsServiceServer) Register(server grpc.ServiceRegistrar) {
+	smsv1.RegisterSmsServiceServer(server, s)
+}
+
+func (s *SmsServiceServer) Send(ctx context.Context, req *smsv1.SmsSendRequest) (*smsv1.SmsSendResponse, error) {
+	err := s.svc.Send(ctx, req.TplId, req.Args, req.Numbers...)
+	return &smsv1.SmsSendResponse{}, err
+}
