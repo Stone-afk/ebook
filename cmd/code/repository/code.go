785b2cb073ddee7bd5bb9688058e2deaec9a1889
Stone-afk <1711865140@qq.com>
2024-04-20 17:53:03 +0800

    feature: add code

--- cmd/code/repository/code.go
+++ cmd/code/repository/code.go
@@ -1,1 +1,24 @@
 package repository
+
+import (
+	"context"
+	"ebook/cmd/code/repository/cache"
+)
+
+type CachedCodeRepository struct {
+	cache cache.CodeCache
+}
+
+func NewCodeRepository(c cache.CodeCache) CodeRepository {
+	return &CachedCodeRepository{
+		cache: c,
+	}
+}
+
+func (repo *CachedCodeRepository) Store(ctx context.Context, biz string, phone string, code string) error {
+	return repo.cache.Set(ctx, biz, phone, code)
+}
+
+func (repo *CachedCodeRepository) Verify(ctx context.Context, biz, phone, inputCode string) (bool, error) {
+	return repo.cache.Verify(ctx, biz, phone, inputCode)
+}
