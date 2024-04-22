3590764e7b84396b5ea7a41fc1f7a95d027b75c7
Stone-afk <1711865140@qq.com>
2024-04-22 16:47:13 +0800

    feature: add code

--- cmd/code/ioc/grpc.go
+++ cmd/code/ioc/grpc.go
@@ -1,1 +1,28 @@
 package ioc
+
+import (
+	grpc2 "ebook/cmd/code/grpc"
+	"ebook/cmd/pkg/grpcx/interceptors/logging"
+	"ebook/cmd/pkg/grpcx/server"
+	"ebook/cmd/pkg/logger"
+	"github.com/spf13/viper"
+	etcdv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/grpc"
+)
+
+func InitGRPCServer(l logger.Logger, etcdClient *etcdv3.Client, codeServer *grpc2.CodeServiceServer) *server.Server {
+	type Config struct {
+		Port    int   `yaml:"port"`
+		EtcdTTL int64 `yaml:"etcdTTL"`
+	}
+	var cfg Config
+	err := viper.UnmarshalKey("grpc.server", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	grpcSrv := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		logging.NewLoggerInterceptorBuilder(l).Build(),
+	))
+	codeServer.Register(grpcSrv)
+	return server.NewGRPCXServer(grpcSrv, etcdClient, l, cfg.Port, "code", cfg.EtcdTTL)
+}
