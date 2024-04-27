b650838790532b5df9827a856506e686b77ebe44
Stone-afk <1711865140@qq.com>
2024-04-27 13:24:53 +0800

    feature: add oauth2

--- cmd/oauth2/ioc/service.go
+++ cmd/oauth2/ioc/service.go
@@ -1,1 +1,20 @@
 package ioc
+
+import (
+	wechat2 "ebook/cmd/oauth2/service/wechat"
+	"ebook/cmd/pkg/logger"
+	"github.com/spf13/viper"
+)
+
+func InitService(logv1 logger.Logger) wechat2.Service {
+	type Config struct {
+		AppID     string `yaml:"appId"`
+		AppSecret string `yaml:"appSecret"`
+	}
+	var cfg Config
+	err := viper.UnmarshalKey("weChatConf", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	return wechat2.NewService(cfg.AppID, cfg.AppSecret, logv1)
+}
