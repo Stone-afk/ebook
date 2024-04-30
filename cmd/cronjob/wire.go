9d9a48a7f7c6724b650160d0a7ca98ddf9b09fc0
Stone-afk <1711865140@qq.com>
2024-04-30 11:56:43 +0800

    feature: add cron job

--- cmd/cronjob/wire.go
+++ cmd/cronjob/wire.go
@@ -1,1 +1,36 @@
+//go:build wireinject
+
 package main
+
+import (
+	"ebook/cmd/cronjob/grpc"
+	"ebook/cmd/cronjob/ioc"
+	"ebook/cmd/cronjob/repository"
+	"ebook/cmd/cronjob/repository/dao"
+	"ebook/cmd/cronjob/service"
+	"ebook/cmd/pkg/appx"
+	"github.com/google/wire"
+)
+
+var serviceProviderSet = wire.NewSet(
+	dao.NewGORMJobDAO,
+	repository.NewPreemptCronJobRepository,
+	service.NewCronJobService)
+
+var thirdProvider = wire.NewSet(
+	ioc.InitDB,
+	ioc.InitLogger,
+	ioc.InitEtcdClient,
+)
+
+//go:generate wire
+func Init() *appx.App {
+	wire.Build(
+		thirdProvider,
+		serviceProviderSet,
+		grpc.NewCronJobServiceServer,
+		ioc.InitGRPCServer,
+		wire.Struct(new(appx.App), "GRPCServer"),
+	)
+	return new(appx.App)
+}
