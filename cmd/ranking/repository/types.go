064d4ff4a41a22f17c143639c4f8842c3e8e3464
Stone-afk <1711865140@qq.com>
2024-05-09 11:06:19 +0800

    feature: add cron job

--- cmd/ranking/repository/types.go
+++ cmd/ranking/repository/types.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+	"context"
+	"ebook/cmd/ranking/domain"
+)
+
+type RankingRepository interface {
+	ReplaceTopN(ctx context.Context, arts []domain.Article) error
+	GetTopN(ctx context.Context) ([]domain.Article, error)
+}
