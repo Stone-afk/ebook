3eaaae47c704ff13d83076a71369371db5a3524b
Stone-afk <1711865140@qq.com>
2024-05-12 10:56:28 +0800

    feature: add ranking

--- cmd/ranking/ioc/grpc.go
+++ cmd/ranking/ioc/grpc.go
@@ -1,1 +1,27 @@
 package ioc
+
+import (
+	"ebook/cmd/pkg/grpcx/server"
+	"ebook/cmd/pkg/logger"
+	grpc2 "ebook/cmd/ranking/grpc"
+	"github.com/spf13/viper"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/grpc"
+)
+
+func InitGRPCxServer(reward *grpc2.RankingServiceServer,
+	etcdClient *clientv3.Client,
+	l logger.Logger) *server.Server {
+	type Config struct {
+		Port    int   `yaml:"port"`
+		EtcdTTL int64 `yaml:"etcdTTL"`
+	}
+	var cfg Config
+	err := viper.UnmarshalKey("grpc.server", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	grpcSrv := grpc.NewServer()
+	reward.Register(grpcSrv)
+	return server.NewGRPCXServer(grpcSrv, etcdClient, l, cfg.Port, "ranking", cfg.EtcdTTL)
+}
