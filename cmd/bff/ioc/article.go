9077a4e8e0b062e1839d90aa7204e45cf2014e79
Stone-afk <1711865140@qq.com>
2024-05-21 12:06:32 +0800

    feature: add bff

--- cmd/bff/ioc/article.go
+++ cmd/bff/ioc/article.go
@@ -1,1 +1,35 @@
 package ioc
+
+import (
+	articlev1 "ebook/cmd/api/proto/gen/article/v1"
+	"github.com/spf13/viper"
+	etcdv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/naming/resolver"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func InitArticleClient(etcdClient *etcdv3.Client) articlev1.ArticleServiceClient {
+	type config struct {
+		Target string `json:"target"`
+		Secure bool   `json:"secure"`
+	}
+	var cfg config
+	err := viper.UnmarshalKey("grpc.client.article", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	rs, err := resolver.NewBuilder(etcdClient)
+	if err != nil {
+		panic(err)
+	}
+	opts := []grpc.DialOption{grpc.WithResolvers(rs)}
+	if !cfg.Secure {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+	conn, err := grpc.Dial(cfg.Target, opts...)
+	if err != nil {
+		panic(err)
+	}
+	return articlev1.NewArticleServiceClient(conn)
+}
